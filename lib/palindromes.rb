5aced9975c9d147edcfb9562299c68bd050920cc
wh0pper <jaredclemmensen@gmail.com>
2018-02-07 13:30:55 -0800

    add logic and tests for additional behaviors

--- lib/palindromes.rb
+++ lib/palindromes.rb
@@ -1,9 +1,24 @@
 class Pals
   def initialize(input)
-    @input = input
+    @input = input.to_s
   end
 
-  def to_letters
-    @input.split(%r{\s*})
+  # def to_letters
+  #   @input.split(%r{\s*})
+  # end
+
+  def is_pal?
+    letters = @input.split(%r{\s*})
+    rev_index = letters.length-1
+    index = 0
+    while index < (letters.length/2)
+      if !(letters[index] == letters[rev_index])
+        return false
+      else
+        index += 1
+        rev_index -=1
+      end
+    end
+    return true
   end
 end
--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -1,14 +1,31 @@
 require('rspec')
 require('palindromes')
 
+# describe ('setup methods') do
+#   it "splits input phrase or word into array of characters minus spaces" do
+#     text = Pals.new('racecar')
+#     expect((text.to_letters() - ['r','a','c','e','c','a','r']).empty?)
+#   end
+#
+#   it "isolates letters in phrase of words" do
+#     text = Pals.new('lonely tylenol')
+#     expect((text.to_letters() - ['l','o','n','e','l','y','t','y','l','e','n','o','l']).empty?)
+#   end
+# end
+
 describe ('#is_pal') do
-  it "splits input phrase or word into array of characters minus spaces" do
+  it "tests for single world palindromes" do
     text = Pals.new('racecar')
-    expect((text.to_letters() - ['r','a','c','e','c','a','r']).empty?)
+    expect(text.is_pal?()).to(eq(true))
   end
 
-  it "isolates letters in phrase of words" do
+  it "tests for single multi word palindromes" do
     text = Pals.new('lonely tylenol')
-    expect((text.to_letters() - ['l','o','n','e','l','y','t','y','l','e','n','o','l']).empty?)
+    expect(text.is_pal?()).to(eq(true))
+  end
+
+  it "tests for integer palindromes" do
+    text = Pals.new(12321)
+    expect(text.is_pal?()).to(eq(true))
   end
 end
